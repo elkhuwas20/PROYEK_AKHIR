80a71a99d71bb8386cc1cfe9e802e882eeb7278c
Nuril Akmal <akmalnuril65@gmail.com>
2025-11-12 16:16:25 +0800

    kedua

---
+++ admin_menu.py
@@ -0,0 +1,16 @@
+def menu_admin ():
+    print("Admin Menu:")
+    print("1. User Management")
+    print("2. System Settings")
+    print("3. View Logs")
+    print("4. Exit")
+    choice = input("Select an option (1-4): ")
+    return choice
+def menu_user_management():
+    print("User Management Menu:")
+    print("1. Add User")
+    print("2. Remove User")
+    print("3. List Users")
+    print("4. Back to Admin Menu")
+    choice = input("Select an option (1-4): ")
+    return choice
