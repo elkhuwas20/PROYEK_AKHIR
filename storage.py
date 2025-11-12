06206bd6b2b23c4e867db49de1e29bdb5d4cfbfd
JulpaST <stjulpa2004@gmail.com>
2025-11-12 13:55:11 +0700

    Add files via upload
    PERTAMA

---
+++ storage.py
@@ -0,0 +1,24 @@
+#LIST USERNAME DAN PASSWORD TERDAFTAR
+admin_usn=["blacklist"]
+admin_pw=["123"]
+user_usn=["julpa", "kikan", "akmal", "doni"]
+user_pw=["321"]
+
+#DICTIONARY DRAMA KOREA3
+drama_korea = {
+"Taxi Driver" : {"genre" : "Crime", "episode" : "16", "rating" : "8.8", "status" : "Finish"},
+"Vincenzo" : {"genre" : "Crime", "episode" : "20", "rating" : "8.9", "status" : "Finish"},
+"Squid Game" : {"genre" : "Thriller", "episode" : "9", "rating" : "8.4", "status" : "Finish"},
+"Goblin" : {"genre" : "Romance", "episode" : "16", "rating" : "8.8", "status" : "Finish"},
+"Business Proposal" : {"genre" : "Romance", "episode" : "12", "rating" : "8.7", "status" : "Finish"},
+"Weak Hero Class" : {"genre" : "Action", "episode" : "8", "rating" : "9.1", "status" : "Finish"},
+"Itaewon Class" : {"genre" : "Drama", "episode" : "16", "rating" : "8.4", "status" : "Finish"},
+"Mr Queen" : {"genre" : "Historical", "episode" : "20", "rating" : "8.6", "status" : "Finish"},
+"Twinkling Watermelon" : {"genre" : "Youth", "episode" : "16", "rating" : "9.2", "status" : "Finish"},
+"Hotel del Luna" : {"genre" : "Horror", "episode" : "16", "rating" : "8.6", "status" : "Finish"},
+"Spirit Fingers" : {"genre" : "Romance", "Episode" : "12", "rating" : "8.5", "status" : "Ongoing"},
+"Would You Marry Me?" : {"genre" : "Romance", "Episode" : "12", "rating" : "8.0", "status" : "Ongoing"},
+"Dear X" : {"genre" : "Thriller", "Episode" : "12", "rating" : "8.5", "status" : "Ongoing"},
+"Typhoon Family" : {"genre" : "Drama", "Episode" : "16", "rating" : "8.1", "status" : "Ongoing"},
+"The Manipulated" : {"genre" : "Action", "Episode" : "12", "rating" : "8.5", "status" : "Ongoing"}
+}
